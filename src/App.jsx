ddf3c3f9e13fdf7af94d9ea5fca19d9dab22ea47
hiashraful <hiashraful@gmail.com>
2023-07-16 22:00:12 +0600

    deleted unnecessary practice functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,17 @@
 import './style.scss';
 import { useState } from 'react';
-// import Board from './components/board';
+import Board from './components/board';
 
 function App() {
-  const [counter, counterUpdate] = useState(new Date().toLocaleTimeString());
-  const btnClick = () => {
-    const timerId = setInterval(() => {
-      counterUpdate(val => {
-        return new Date().toLocaleTimeString();
-      });
-    }, 1000);
-  };
-  
   return (
-    <div className="app">
-      <button type="button" onClick={btnClick}>
-        Click me
-      </button>
-      <h2>{counter}</h2>
-    </div>
+    <>
+      <div className="app">
+        <button type="button" onClick={btnClick}>
+          Click me
+        </button>
+        <h2>{counter}</h2>
+      </div>
+    </>
   );
 }
 
